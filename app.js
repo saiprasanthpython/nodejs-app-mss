17d548f7275c56efb84b605eeb22a1709d4837a7
saiprasanthpython <103930396+saiprasanthpython@users.noreply.github.com>
2022-04-28 17:17:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,11 +19,11 @@ res.send("<h2>Welcome to Node JS express app</h2>"+appEnv.url+appEnv.port+port+p
 }).listen(9009);
 console.log(__dirname+"/images/mithunlogo.jpg");
 */
-app.get('/mithuntechnologies', function(request, response) {
+app.get('/sidhudevops', function(request, response) {
     //response.send("<h2><center>Welcome to Node JS app</h2>");
     response.write("<h2><center><u>Node JS  Application </u></center></h2>");
 	
-    response.write("<h2><center>Welcome to  Mithun Technologies. Please Contact +91-9980923226 for more information or send an email to devopstrainingblr@gmail.com <center></h2>" );
+    response.write("<h2><center>Welcome to  sidhudevopswebsite. Please Contact +91-7619632475 for more information or send an email to saiprasanthpython@gmail.com <center></h2>" );
     response.end();
     
   })
